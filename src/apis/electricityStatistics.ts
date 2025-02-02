0a5ae4b03d85aa751fdb2720642b02e068513d8b
Apluri <henri.hietala95@gmail.com>
2025-02-02 20:38:43 +0200

    feat: create daily statistics list (#3)
    Co-authored-by: henri-hietala <henri.hietala@futurice.com>

--- src/apis/electricityStatistics.ts
+++ src/apis/electricityStatistics.ts
@@ -4,8 +4,9 @@ import {
   MRT_SortingState,
 } from "material-react-table";
 import {
+  DailyElectricityDataDTO,
   ElectricityData,
-  ElectricityDataResponse as ElectricityDataDTO,
+  ElectricityDataDTO as ElectricityDataDTO,
 } from "../models/electricity";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 
@@ -53,6 +54,48 @@ export const useGetElectricityData = (
         meta: json.meta,
       };
 
+      return convertedData;
+    },
+    placeholderData: keepPreviousData, //don't go to 0 rows when refetching or paginating to next page
+  });
+};
+
+export const useGetDailyElectricityData = (
+  columnFilters: MRT_ColumnFiltersState,
+  pagination: MRT_PaginationState,
+  sorting: MRT_SortingState
+) => {
+  return useQuery<DailyElectricityDataDTO>({
+    queryKey: ["electricityData", { columnFilters, pagination, sorting }],
+    queryFn: async () => {
+      const fetchURL = new URL("/api/statistics/daily", BASE_URL);
+
+      fetchURL.searchParams.set(
+        "pageStart",
+        `${pagination.pageIndex * pagination.pageSize}`
+      );
+      fetchURL.searchParams.set("pageSize", `${pagination.pageSize}`);
+      fetchURL.searchParams.set("filters", JSON.stringify(columnFilters ?? []));
+      fetchURL.searchParams.set("sorting", JSON.stringify(sorting ?? []));
+
+      const response = await fetch(fetchURL.href);
+      const json: DailyElectricityDataDTO = await response.json();
+
+      const convertedData: DailyElectricityDataDTO = {
+        data: json.data.map((data) => {
+          return {
+            ...data,
+            date: (() => {
+              if (data.date) {
+                return new Date(data.date);
+              } else {
+                throw new Error("Date is required");
+              }
+            })(),
+          };
+        }),
+        meta: json.meta,
+      };
       return convertedData;
     },
     placeholderData: keepPreviousData, //don't go to 0 rows when refetching or paginating to next page
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,14 @@
 import { Box, Typography } from "@mui/material";
-import StatisticsList from "../components/StatisticsList";
+import ListRaw from "../components/ListRaw";
+import DailyStatisticsList from "../components/DailyStatisticsList";
 
 const LandingPage = () => {
   return (
     <Box>
-      <Typography variant="h1">Electricity Statistics</Typography>
-      <StatisticsList />
+      <Typography variant="h1">Daily electricity Statistics</Typography>
+      <DailyStatisticsList />
+      <Typography variant="h2">Raw Data</Typography>
+      <ListRaw />
     </Box>
   );
 };
