a22f6fc5c00a15fae66f70706564a6d28d1eb2d3
Apluri <henri.hietala95@gmail.com>
2025-02-04 16:15:57 +0200

    fix: hotfix for timezones (#9)
    Co-authored-by: henri-hietala <henri.hietala@futurice.com>

--- src/helpers/dataParsing.ts
+++ src/helpers/dataParsing.ts
@@ -1,3 +1,5 @@
+import { fromZonedTime } from "date-fns-tz";
+import { MRT_ColumnFiltersState } from "material-react-table";
 import { z } from "zod";
 
 /**
@@ -12,3 +14,26 @@ export const stringOrNullToNumber = z
     const num = Number(val);
     return isNaN(num) ? undefined : num;
   });
+
+/**
+ * Transform Date values to UTC. Others keep as is.
+ * @param filters MRTable filters
+ * @returns New list of filters with Date values transformed to UTC
+ */
+export const formatDateValuesToUTC = (filters: MRT_ColumnFiltersState) => {
+  return filters.map((filter) => {
+    if (Array.isArray(filter.value)) {
+      const [start, end] = filter.value;
+      const startDate = start instanceof Date ? start : null;
+      const endDate = end instanceof Date ? end : null;
+      return {
+        ...filter,
+        value: [
+          startDate ? fromZonedTime(startDate, "UTC").toISOString() : start,
+          endDate ? fromZonedTime(endDate, "UTC").toISOString() : end,
+        ],
+      };
+    }
+    return filter;
+  });
+};
